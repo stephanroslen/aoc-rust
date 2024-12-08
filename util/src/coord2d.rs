c0ed7ec60be81e697f573270eed90d3706e7a5cd
Stephan Roslen <stephan@roslen.de>
2024-12-08 10:06:26 +0100

    day08

--- util/src/coord2d.rs
+++ util/src/coord2d.rs
@@ -1,6 +1,6 @@
 use crate::error::Errors;
-use std::ops::Add;
 use std::ops::Sub;
+use std::ops::{Add, SubAssign};
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub struct UCoord2D {
@@ -44,6 +44,33 @@ impl Add<ICoord2D> for UCoord2D {
     }
 }
 
+impl Add<ICoord2D> for ICoord2D {
+    type Output = ICoord2D;
+    fn add(self, rhs: ICoord2D) -> Self::Output {
+        Self::Output {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub<ICoord2D> for ICoord2D {
+    type Output = ICoord2D;
+    fn sub(self, rhs: ICoord2D) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl SubAssign<ICoord2D> for ICoord2D {
+    fn sub_assign(&mut self, rhs: ICoord2D) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
 impl TryInto<UCoord2D> for ICoord2D {
     type Error = crate::error::Errors;
 
@@ -54,6 +81,21 @@ impl TryInto<UCoord2D> for ICoord2D {
             ICoord2D { x, y } => Ok(UCoord2D {
                 x: x as usize,
                 y: y as usize,
+            }),
+        }
+    }
+}
+
+impl TryInto<ICoord2D> for UCoord2D {
+    type Error = crate::error::Errors;
+
+    fn try_into(self) -> Result<ICoord2D, Self::Error> {
+        match self {
+            UCoord2D { x, y: _ } if x > isize::MAX as usize => Err(Errors::ConversionError),
+            UCoord2D { x: _, y } if y > isize::MAX as usize => Err(Errors::ConversionError),
+            UCoord2D { x, y } => Ok(ICoord2D {
+                x: x as isize,
+                y: y as isize,
             }),
         }
     }
